b388a5259cb4e7eaf8ed4fe7ed27af65b2471354
Iain <iain@zora.co>
2022-05-01 10:57:06 -0400

    add condom info

--- pages/api/metadata/condom/[id].js
+++ pages/api/metadata/condom/[id].js
@@ -1,12 +1,24 @@
-import sql from '../../../../services/metadata-db';
+import sql from '../../../../services/metadata-db'
+
+const DEFAULT_METADATA = {
+  title: `Condom For Your Wallet`,
+  description: `To celebrate the season finale of Proof of Love S0 of the Mad Realities network, we invite you to mint a condom NFT for your wallet. 
+Participate in the finale to unlock a shiny condom by attending the aftershow filming IRL or by voting for the winning couple online. Shiny condoms provide secret perks and access to be revealed over time.
+Minting is open for 72 hours after doors open (6:30pm ET on May 1, 2022) and cements your status as an early supporter of Mad Realities.
+https://madrealities.xyz/`,
+  properties: {
+    Status: 'Broken',
+    Interaction: 'Powers Dormant',
+  },
+}
 
 export default async function handler(req, res) {
   const { id } = req.query
 
-  const idNumber = parseInt(id, 10);
+  const idNumber = parseInt(id, 10)
 
   if (!sql) {
-    res.send({has: false});
+    res.send(DEFAULT_METADATA)
     return
   }
 
@@ -15,9 +27,15 @@ export default async function handler(req, res) {
   `
 
   if (result) {
-    res.send({has: true});
+    res.send({
+      ...DEFAULT_METADATA,
+      properties: {
+        Status: 'Sexy',
+        Interaction: 'Powers Unlocked',
+      },
+    })
     return
   }
 
-  res.send({has: true});
+  res.send(DEFAULT_METADATA)
 }
