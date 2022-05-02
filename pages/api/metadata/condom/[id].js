b19c1e2a19ad99992424595029a3845ba0499eb3
Iain <iain@zora.co>
2022-05-01 21:32:38 -0400

    metadata update

--- pages/api/metadata/condom/[id].js
+++ pages/api/metadata/condom/[id].js
@@ -6,9 +6,11 @@ const DEFAULT_METADATA = {
 
 Condoms for your wallet were distributed for free starting May 1, 2022 to commemorate your participation in the wrap of Season Zero of Proof of Love, an interactive dating show where you decide who (and what) happens.
 
-This condom provides secret perks and access to be revealed over time, and might unwrap into something sexy.`,
+This condom is our way of giving our growing, local community their first NFT and limited summer 2022 access to Mad Realities festivities, and is our way of knowing who has participated in the universe we've created this Spring.
+
+if u hold this ur sexy, we don't make the rules`,
   image: 'ipfs://QmQxjDGdfqb88kCc4tfrxTo3DZgrBFrj1omi63oynJkE2n',
-  animation_url: 'ipfs://QmP2ZV51w5VSPa1s3iRqo6NpayJTzjnNihpkcjGjgZ5e8H',
+  animation_url: 'ipfs://QmQRu24FiPGvuZ3u7nx9ZfV52koPuW5CuMLkdTo2uzhPao',
   properties: {
     Status: 'Broken',
     Interaction: 'Powers Dormant',
@@ -24,6 +26,15 @@ export default async function handler(req, res) {
     ...DEFAULT_METADATA,
     name: `${DEFAULT_METADATA.name} #${id}`,
   }
+
+  res.send({
+    ...metadataValue,
+    properties: {
+      Status: 'Sexy',
+      Interaction: 'Powers Unlocked',
+    },
+  })
+  return
 
   if (!sql) {
     res.send(metadataValue)
